7803bcc4b203d94bbef4701e84bdae59cb7bc5c7
CodinGitHub <gms2programador@gmail.com>
2022-02-24 03:53:24 -0500

    Explicado en video

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Seleccionando Elementos
+// Lectura de Nodos
 
 // Por ID
 // let elementoPorId = document.getElementById('parrafo1');
@@ -8,9 +8,12 @@ elementoPorId.innerHTML = 'Suscríbete';
 // Por nombre de la clase
 // let elementosPorClase = document.getElementsByClassName('parrafos');
 let elementosPorClase = document.querySelectorAll('.parrafos');
-elementosPorClase[1].innerHTML = 'a';
+console.log(elementosPorClase);
+const arreglo = [...elementosPorClase];
+console.log(arreglo);
+arreglo[1].innerHTML = 'a CodingTube';
 
 // Por etiqueta
 // let elementosPorEtiquetas = document.getElementsByTagName('p');
 let elementosPorEtiquetas = document.querySelectorAll('p');
-elementosPorEtiquetas[2].innerHTML = 'CodingTube';
+elementosPorEtiquetas[2].innerHTML = 'ahora!'
